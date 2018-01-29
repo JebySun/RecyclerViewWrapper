c2eacacca1104d2b2207ef756adb747301d0c4ed
Jeby Sun <jebysun@126.com>
2018-01-29 22:58:14 +0800

    项目升级到Android Studio 3

--- app/src/main/java/com/jebysun/demo/MainActivity.java
+++ app/src/main/java/com/jebysun/demo/MainActivity.java
@@ -4,27 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.jebysun.demo.test.FrameTestActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView mTvWrapper;
+    private Button mBtnWrapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTvWrapper = (TextView) findViewById(R.id.tv_wrapper);
-        mTvWrapper.setOnClickListener(this);
+        mBtnWrapper = findViewById(R.id.btn_wrapper);
+        mBtnWrapper.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        if (v == mTvWrapper) {
+        if (v == mBtnWrapper) {
             startActivity(new Intent(this, WrapperRecyclerViewTestActivity.class));
 //            startActivity(new Intent(this, FrameTestActivity.class));
         }
